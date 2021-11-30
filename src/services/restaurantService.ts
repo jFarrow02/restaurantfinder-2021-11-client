6ca237633deb238770ce7b250554c62d1dbe2326
jFarrow02 <jack.dempsey.farrow@gmail.com>
2021-11-29 21:32:16 -0500

    initial commit

---
+++ src/services/restaurantService.ts
@@ -0,0 +1,14 @@
+const RestaurantService = {
+
+    findAllRestaurants():void {},
+
+    findRestaurantsByBorough(borough: string): void {},
+
+    findRestaurantByName(name: string): void {},
+
+    findRestaurantsByCuisineType(type: string): void {},
+
+    findRestaurantsByAverageGrade(grade: string): void {},
+}
+
+export default RestaurantService;
