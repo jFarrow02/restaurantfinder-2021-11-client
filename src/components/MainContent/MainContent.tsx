efdbd7519ce5854adf25e7e39f1b825ef5be1e55
jFarrow02 <jack.dempsey.farrow@gmail.com>
2021-12-01 19:55:32 -0500

    implicit rows

--- src/components/MainContent/MainContent.tsx
+++ src/components/MainContent/MainContent.tsx
@@ -7,17 +7,27 @@ export default () => {
         <section className="MainContent">
             <div className="banner">MAIN CONTENT</div>
             <>
+                <div className="vertical">
+                    <Restaurant/>
+                </div>
+                <Restaurant/>
+                <div className="horizontal">
+                    <Restaurant/>
+                </div>
                 <Restaurant/>
                 <Restaurant/>
+                <div className="horizontal">
+                    <Restaurant/>
+                </div>
+                <Restaurant/>
+                <div className="vertical">
+                    <Restaurant/>
+                </div>
                 <Restaurant/>
                 <Restaurant/>
-                <Restaurant/>
-                <Restaurant/>
-                <Restaurant/>
-                <Restaurant/>
-                <Restaurant/>
-                <Restaurant/>
-                <Restaurant/>
+                <div className="horizontal">
+                    <Restaurant/>
+                </div>
                 <Restaurant/>
                 <Restaurant/>
                 <Restaurant/>
