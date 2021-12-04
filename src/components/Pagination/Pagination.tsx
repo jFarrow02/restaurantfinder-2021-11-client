ee0df3d7943958cd2db90db6c0eff0b6034e3e85
jFarrow02 <jack.dempsey.farrow@gmail.com>
2021-12-04 12:04:39 -0500

    displays page numbers on abc select OK

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -10,40 +10,58 @@ interface PaginationPropsInterface {
 const Pagination = (props:PaginationPropsInterface):JSX.Element => {
 
     const [ currentPage, setCurrentPage ] = useState('a');
+    const [ currentPageNumber, setCurrentPageNumber ] = useState(1);
+    const [ currentResultsByPageLength, setCurrentResultsByPageLength ] = useState(0);
+    const [ totalResultsLength, setTotalResultsLength ] = useState(0);
+
+
     const { ALPHABET, RESULTS_PER_PAGE } = config;
     const {
         restaurantsList
     } = props;
 
-    // const totalPagesNumber = Math.ceil(paginatedResultCount / RESULTS_PER_PAGE);
+    const selectPage = (page:string):void => {
+        setCurrentPage(page);
+        setCurrentPageNumber(1);
+        const currentResultsByPage = restaurantsList.filter((r:RestaurantInterface) => {
+            return currentPage === 'special' ? ALPHABET.indexOf(r.name.substring(0, 1).toLowerCase()) === -1 :
+                r.name.substring(0, 1).toLowerCase() === currentPage;
+        });
+        setCurrentResultsByPageLength(currentResultsByPage.length);
+    };
+
+    const paginateResults = (currentPageNumber:number):void => {
+        setCurrentPageNumber(currentPageNumber);
+
+        
+    }
 
     const links = ALPHABET.map((char, idx) => {
         return(
             <button
                 key={`pagination-${idx}`}
-                onClick={() => {setCurrentPage(char)}}
+                onClick={() => {selectPage(char)}}
             >
                 {char.toUpperCase()}
             </button>
         );
     });
 
-    // const numbers: JSX.Element[] = [];
-    // for(let i = 0; i < totalPagesNumber; i+= 1) {
-    //     numbers.push(
-    //     <button 
-    //         key={`page-number-${i + 1}`}
-    //         onClick={() => {paginateRestaurants(currentPage, i + 1)}}
-    //     >
-    //         {i + 1}
-    //     </button>);
-    // };
+    const numbers: JSX.Element[] = [];
+    for(let i = 0; i < Math.ceil(currentResultsByPageLength / RESULTS_PER_PAGE); i+= 1) {
+        numbers.push(
+        <button 
+            key={`page-number-${i + 1}`}
+            onClick={() => {paginateResults(i + 1)}}
+        >
+            {i + 1}
+        </button>);
+    };
 
     return(
         <section className="Pagination">
             {links}
-            {/* {links}
-            {numbers} */}
+            {numbers}
         </section>
     )
 };
